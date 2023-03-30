5c515097073559305e98871a745ba897ca6ae2a9
AliS81R <aternum2018@bk.ru>
2023-03-30 19:23:19 +0300

    Preset added

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import UnoCSS from "unocss/vite";
+import presetUno from "@unocss/preset-uno";
+export default {
+  plugins: [
+    UnoCSS({
+      presets: presetUno(), // disable default preset
+      rules: [
+        // your custom rules
+      ],
+    }),
+  ],
+};
