9984357b2c56bd8c9560e12d4f51d0526e2c5ac8
AliS81R <aternum2018@bk.ru>
2023-04-07 22:10:09 +0300

    change 2

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
 import "uno.css";
 import "@unocss/reset/tailwind.css";
+
+
+const domBtnCreateTask = document.getElementById('btnCreateTask');
+const domPopupCreateTask = document.getElementById('popupCreateTask');
+
+domBtnCreateTask.onclick = () => {
+    console.log('click');
+    domPopupCreateTask.classList.remove('hidden');
+
+    const domBtnCloseCreateTaskPopup = document.getElementById('btnCloseCreateTaskPopup');
+
+    domBtnCloseCreateTaskPopup.onclick = () => {
+        domPopupCreateTask.classList.add('hidden');
+        domBtnCloseCreateTaskPopup.onclick = null;
+    };
+
+};
+
+
+const domNewTodoTask = document.getElementById('TodoTask');
+const domBtnCreateNewTask = document.getElementById('btnCreateNewTask');
+
+ let tasks = [];
+let todoTasks = [];
+
+if (!localStorage.tasks) {
+    return tasks;
+} else {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+}
+
+
+
