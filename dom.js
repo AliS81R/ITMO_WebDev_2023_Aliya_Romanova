871059a04af60431fc0936cceb1832231695df1e
AliS81R <aternum2018@bk.ru>
2023-04-13 18:48:06 +0300

    added function-add title and date 1

---
+++ dom.js
@@ -0,0 +1,27 @@
+const DOM = {
+  Button: {
+    CREATE_TASK: "btnCreateTask",
+    POPUP_CREATE_TASK_CLOSE: "btnCloseCreateTaskPopup",
+    POPUP_CREATE_TASK_CONFIRM: "btnConfirmTaskPopup",
+  },
+  Template: {
+    TASK: "templateTask",
+    Task: {
+      TITLE: "templateTaskTitle",
+    },
+  },
+  Popup: {
+    CREATE_TASK: "popupCreateTask",
+    Input: {
+      INFO_TITLE: "inpTitle",
+      INFO_DATE: "inpDate",
+      USER_DATE: "userDate",
+      INFO_TAG: "countries",
+    },
+    CreateTask: {
+      TITLE: "title",
+    },
+  },
+};
+
+export default DOM;
