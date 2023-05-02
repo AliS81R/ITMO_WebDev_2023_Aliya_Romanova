31dd815fab3f940b998bf9232a8518672bd238a2
AliS81R <aternum2018@bk.ru>
2023-05-02 21:21:14 +0300

    2

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "red";
-ctx.beginPath();
-ctx.arc(150, 50, 50, 0, Math.PI * 2);
-ctx.fill();
+class Position {
+  x;
+  y;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+let x = 0,
+  y = 0;
+
+const render = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "red";
+  ctx.beginPath();
+  ctx.arc(x, y, 50, 0, Math.PI * 2);
+  ctx.fill();
+  x += 1;
+  if (x > canvas.width) {
+    x = 0;
+  }
+};
+
+window.requestAnimationFrame();
