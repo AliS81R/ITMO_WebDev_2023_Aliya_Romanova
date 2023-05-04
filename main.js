e46c2d687af08815199b3de4240e5d1f37b01ec9
AliS81R <aternum2018@bk.ru>
2023-05-04 18:53:03 +0300

    2

--- main.js
+++ main.js
@@ -1,26 +1,62 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-class Position {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Planet {
   x;
   y;
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+  size;
+  pX;
+  pY;
+  atmosphere;
+  radius;
+  alpha;
+  speed;
+  constructor(x, y, speed = 0.1, size = 10, atmosphere = "red", radius = 50) {
+    this.size = size;
+    this.pX = x;
+    this.pY = y;
+    this.atmosphere = atmosphere;
+    this.radius = radius;
+    this.speed = speed;
+    this.alpha = 0;
+  }
+
+  move() {
+    this.x = this.radius * Math.sin(this.alpha) + this.pX;
+    this.y = this.radius * Math.cos(this.alpha) + this.pY;
+    this.alpha += (this.speed * Math.PI) / 180;
+  }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.atmosphere;
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
   }
 }
-const R = 30;
-let x = 0,
-  y = 0;
+
+const planets = [
+  new Planet(200, 200, 0.1, "red", 50),
+  new Planet(200, 200, 0.2, "blue", 100),
+  new Planet(200, 200, 0.3, "green", 150),
+  new Planet(200, 200, 0.3, "gold", 200),
+];
+
+let planet;
 
 const render = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "red";
-  ctx.beginPath();
-  ctx.arc(x, y, 50, 0, Math.PI * 2);
-  ctx.fill();
-  x = R * Math.sin(alpha);
-  y = R * Math.cos(alpha);
+
+  for (const planetIndex in planets) {
+    planet = planets[planetIndex];
+    planet.move();
+    planet.render(ctx);
+  }
+  window.requestAnimationFrame(render);
 };
 
-window.requestAnimationFrame();
+window.requestAnimationFrame(render);
