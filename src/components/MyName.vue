23fb8a5ba03c84874e67aa5bcaf3bcac94d1834b
AliS81R <aternum2018@bk.ru>
2023-05-06 13:21:31 +0300

    vue inite project

---
+++ src/components/MyName.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Enter you name</h1>
+  <div>
+    {{ firstName }}
+    <span style="color: red">
+      {{ lastName }}
+    </span> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyName",
+  data: () => ({
+    firstName: 'Aliya',
+    lastName: 'Romanova'
+  })
+};
+</script>
+
+<style scoped>
+
+</style>
