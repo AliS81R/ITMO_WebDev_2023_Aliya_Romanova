013e6a45e7b2024025bab8701efc146e2a9fee1c
AliS81R <aternum2018@bk.ru>
2023-05-22 20:43:11 +0300

    1

--- calculate.js
+++
@@ -1,8 +0,0 @@
-const OPERATIONS = {
-  sum: { method: (a, b) => a + b, title: "+" },
-  sub: { method: (a, b) => a - b, title: "-" },
-  mult: { method: (a, b) => a * b, title: "*" },
-  divd: { method: (a, b) => a / b, title: "/" },
-};
-
-export { OPERATIONS };
