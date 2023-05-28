77a04de41b3f7509264cbe16c407107ec6a86d65
AliS81R <aternum2018@bk.ru>
2023-05-28 18:20:41 +0300

    2

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import AppComposition from './AppComposition.vue';
 
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase(import.meta.env.VITE_SERVER_PATH);
 
 createApp(AppComposition)
   .use(createPinia().use(piniaPluginPersistedState))
