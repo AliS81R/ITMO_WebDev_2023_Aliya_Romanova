d7db924d20aa84f1cc190b84accbc5f77dd44f69
AliS81R <aternum2018@bk.ru>
2023-06-01 19:35:20 +0300

    5

--- src/pages/SigInPage.vue
+++ src/pages/SigInPage.vue
@@ -1,10 +1,21 @@
 <script setup>
 
 import RegistrationForm from '@/components/RegistrationForm.vue';
+import PROVIDE from '@/constants/provides.js';
+
+const pb = inject(PROVIDE.PB);
+const onLogin = (dto) => {
+  pb.collection('users').authWithPassword(
+    'YOUR_USERNAME_OR_EMAIL',
+    'YOUR_PASSWORD',
+  ).then(() => {
+  });
+};
+
 </script>
 <template>
   <div>
-    <RegistrationForm />
+    <RegistrationForm @login="onLogin" />
     <router-link to="ROUTES.SIGNIN">
       Sign In
     </router-link>
--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -3,29 +3,44 @@ import RegistrationForm from '@/components/RegistrationForm.vue';
 import ROUTES from '@/constants/routes.js';
 import { inject, ref } from 'vue';
 import PROVIDE from '@/constants/provides.js';
+import router from '@/route.js';
 
 const errors = ref([]);
 const pb = inject(PROVIDE.PB);
 const onRegister = (dto) => {
   console.log('>SignUpPage --onRegister:', dto);
 
-  if(!dto.password || dto.password.length === 0) {
-    errors.value =['Password required'];
+  if (!dto.password || dto.password.length === 0) {
+    errors.value = ["Password required"];
   } else {
-    pb.collation('users').create({
-      username:dto.username,
-      password:dto.password,
-      passwordConfirm: dto.password,
-    }).catch((error) => {
-      const errorData = error.data.data;
-      for(const item in error.data){
-        const data = errorData[item];
-        console.log('>item', item);
-        errors.value.push(data.message);
+    pb.collation("users").create({
+      username: dto.username,
+      password: dto.password,
+      passwordConfirm: dto.password
+    }).than((record) => {
+      console.log("SignUpPage onRegister: result = ", record);
+      if (window.confirm(`User created with ID: ${record.id}`)) {
+        router.replace({ path: ROUTES.INDEX });
       }
-    });
-  }
-};
+    }).catch((error) => {
+      console.log("SignUpPage onRegister: error = ", { error, errorData);
+      if (errorData) {
+        const errorData = error.data.data;
+        for (const item in errorData) {
+          const data = errorData[item];
+          console.log(">item", data);
+          errors.value.push(data.message);
+
+        }
+      else
+        {
+          errors.value.push(error.message);
+        }
+      }
+    })
+      ;
+    }
+  };
 
 </script>
 <template>
